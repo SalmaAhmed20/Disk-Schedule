4e444086b0cf0375970fa6c031173c133e8920e9
thoraya-atef <63753887+thoraya-atef@users.noreply.github.com>
2021-05-05 12:42:14 +0200

    Add files via upload

--- src/DiskScheduling.java
+++ src/DiskScheduling.java
@@ -9,8 +9,10 @@ public class DiskScheduling {
 
     public void Look(ArrayList<Integer>  Requests,int HeadPos)
     {
+        System.out.println ("\n----------------Look Algorithm --------------------");
        int total_head_movement=0;
        int distance,current_loc;
+       int originalHeadPos=HeadPos;
        ArrayList<Integer> left,right,sequence;
        left=new ArrayList<>();
        right=new ArrayList<>();
@@ -26,43 +28,77 @@ public class DiskScheduling {
         //to sort arr left and right
         Collections.sort(left);
         Collections.sort(right);
-           for(int i = 0; i < right.size(); i++)
-           {
-               current_loc = right.get(i);
-               sequence.add(current_loc);
+        int x=1,diraction_right=1;
+        while (x!=5) {
 
-               // to absolute value
-               distance = Math.abs(current_loc - HeadPos);
+            if(diraction_right==1) {
+                for (int i = 0; i < right.size(); i++) {
+                    current_loc = right.get(i);
+                    sequence.add(current_loc);
 
-               total_head_movement=total_head_movement+ distance;
-               HeadPos = current_loc;
-           }
-           for(int i = left.size() - 1;i >= 0; i--)
-           {
-               current_loc = left.get(i);
-               sequence.add(current_loc);
+                    // to absolute value
+                    distance = Math.abs(current_loc - HeadPos);
 
-               // to absolute value
-               distance = Math.abs(current_loc - HeadPos);
+                    total_head_movement = total_head_movement + distance;
+                    HeadPos = current_loc;
+                }
+                if(x==1) {
+                    diraction_right = 0;
+                }
+            }
+            else if(diraction_right!=1) {
+                for (int i = left.size() - 1; i >= 0; i--) {
+                    current_loc = left.get(i);
+                    sequence.add(current_loc);
 
-               total_head_movement=total_head_movement+ distance;
-               HeadPos = current_loc;
+                    // to absolute value
+                    distance = Math.abs(current_loc - HeadPos);
 
-           }
+                    total_head_movement = total_head_movement + distance;
+                    HeadPos = current_loc;
+                    if(x==3) {
+                        diraction_right = 1;
+                    }
 
-        System.out.println("\n *******************************");
-        System.out.println("Sequence is:- ");
-        for(int i = 0; i < sequence.size(); i++)
-        {
-            System.out.println(sequence.get(i)+" ");
+                }
+
+            }
+            if(x==2)
+            {
+                System.out.println("\n ---------  right direction ---------");
+                System.out.println ("Sequence of head movement:- ");
+                for(int i = 0; i < sequence.size(); i++)
+                {
+                    System.out.println(sequence.get(i)+" ");
+                }
+                System.out.println ("\nThe Total Head Movement To Look Algorithm  " + total_head_movement + " Cylinder");
+                sequence=new ArrayList<>();
+                total_head_movement=0;
+                HeadPos=originalHeadPos;
+            }
+            if(x==4)
+            {
+                System.out.println("\n --------- left direction ---------");
+                System.out.println ("Sequence of head movement:- ");
+                for(int i = 0; i < sequence.size(); i++)
+                {
+                    System.out.println(sequence.get(i)+" ");
+                }
+                System.out.println ("\nThe Total Head Movement To Look Algorithm  " + total_head_movement + " Cylinder");
+            }
+            x++;
+
         }
-        System.out.println("The Total Head Movement To Look Algorithm  " +total_head_movement);
+
+
 
     }
     public void C_Look(ArrayList<Integer>  Requests,int HeadPos)
     {
+        System.out.println ("\n----------------C Look Algorithm --------------------");
         int total_head_movement=0;
         int distance,current_loc;
+        int originalHeadPos=HeadPos;
         ArrayList<Integer> left,right,sequence;
         left=new ArrayList<>();
         right=new ArrayList<>();
@@ -79,40 +115,71 @@ public class DiskScheduling {
         Collections.sort(left);
         Collections.sort(right);
 
+                for (int i = 0; i < right.size(); i++) {
+                    current_loc = right.get(i);
+                    sequence.add(current_loc);
 
-            for(int i = 0; i < right.size(); i++)
-            {
-                current_loc = right.get(i);
-                sequence.add(current_loc);
+                    // to absolute value
+                    distance = Math.abs(current_loc - HeadPos);
 
-                // to absolute value
-                distance = Math.abs(current_loc - HeadPos);
+                    total_head_movement = total_head_movement + distance;
+                    HeadPos = current_loc;
+                }
 
-                total_head_movement=total_head_movement+ distance;
-                HeadPos = current_loc;
-            }
-            for(int i = 0;i < left.size(); i++)
-            {
-                current_loc = left.get(i);
-                sequence.add(current_loc);
+                for (int i = 0; i < left.size(); i++) {
+                    current_loc = left.get(i);
+                    sequence.add(current_loc);
 
-                // to absolute value
-                distance = Math.abs(current_loc - HeadPos);
+                    // to absolute value
+                    distance = Math.abs(current_loc - HeadPos);
 
-                total_head_movement=total_head_movement+ distance;
-                HeadPos = current_loc;
-            }
-        System.out.println("\n *******************************");
-        System.out.println("Sequence is:- ");
+                    total_head_movement = total_head_movement + distance;
+                    HeadPos = current_loc;
+                }
+        System.out.println("\n ---------  right direction ---------");
+        System.out.println ("Sequence of head movement:- ");
         for(int i = 0; i < sequence.size(); i++)
         {
             System.out.println(sequence.get(i)+" ");
         }
-        System.out.println("The Total Head Movement To C_Look Algorithm  " +total_head_movement);
+        System.out.println ("\nThe Total Head Movement To C_Look Algorithm  " + total_head_movement + " Cylinder");
+        sequence=new ArrayList<>();
+        total_head_movement=0;
+        HeadPos=originalHeadPos;
+
+        for (int i = left.size()-1; i >=0 ; i--) {
+            current_loc = left.get(i);
+            sequence.add(current_loc);
+
+            // to absolute value
+            distance = Math.abs(current_loc - HeadPos);
+
+            total_head_movement = total_head_movement + distance;
+            HeadPos = current_loc;
+        }
+        for (int i = right.size()-1; i >=0; i--) {
+            current_loc = right.get(i);
+            sequence.add(current_loc);
+
+            // to absolute value
+            distance = Math.abs(current_loc - HeadPos);
+
+            total_head_movement = total_head_movement + distance;
+            HeadPos = current_loc;
+        }
+
+        System.out.println("\n ---------  left direction ---------");
+        System.out.println ("Sequence of head movement:- ");
+        for(int i = 0; i < sequence.size(); i++)
+        {
+            System.out.println(sequence.get(i)+" ");
+        }
+        System.out.println ("\nThe Total Head Movement To C_Look Algorithm  " + total_head_movement + " Cylinder");
 
     }
     public  void Optimized(ArrayList<Integer> Requests)
     {
+        System.out.println ("\n----------------Optimized Algorithm --------------------");
         Collections.sort(Requests);
         int start=0,current_loc,total_head_movement = 0,distance;
         for(int i = 0; i < Requests.size(); i++)
@@ -126,13 +193,11 @@ public class DiskScheduling {
             start = current_loc;
         }
 
-        System.out.println("\n *******************************");
-        System.out.println("Sequence is:- ");
-        for(int i = 0; i < Requests.size(); i++)
-        {
-            System.out.println(Requests.get(i)+" ");
+        System.out.println ("Sequence of head movement:- ");
+        for (int i = 0 ; i < Requests.size () ; i++) {
+            System.out.println (Requests.get (i) + " ");
         }
-        System.out.println("The Total Head Movement To Optimized Algorithm  " +total_head_movement);
+        System.out.println ("\nThe Total Head Movement To Optimized Algorithm  " + total_head_movement + " Cylinder");
 
 
     }
